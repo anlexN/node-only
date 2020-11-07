190131670c737a0b5a17373364828b5fdf3a260b
anlex N <1293006794@qq.com>
2020-11-07 23:24:58 +0800

    optimize

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
 module.exports = function(obj = {}, keys){
-  if ('string' == typeof keys) keys = keys.split(/ +/);
+  if ('string' == typeof keys) keys = keys.trim().split(/ +/);
 
   return keys.reduce(function(ret, key){
-    // if (null == obj[key]) return ret;
-    // ret[key] = obj[key];
-    // return ret;
     if (null != obj[key]) ret[key] = obj[key];
 
     return ret;
